d7f88b150c4a3cbc6753bbad68b0c3e9a7d6b998
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2020-09-02 09:02:39 +0300

    new internal functions iarccrgtools_dataset, iarccrgtools_tool: collect data for and call iarccrgtools::interact_with_tool

--- R/preprocessing_enrichment.R
+++ R/preprocessing_enrichment.R
@@ -19,25 +19,25 @@
 #' @export
 
 ###enrichment" part of our preprocessing.
-###function for creating columns based on information in the other columns. 
+###function for creating columns based on information in the other columns.
 ###for the moment for the columns other than the entity ones and the ones "Created by R-program from checktool-info"
 
 # ####working directory
 # setwd("")
-# 
+#
 # ####packages
 # packages=c('data.table','remotes')
 # for (p in packages){
 #   if (!require(p,character.only=T)){install.packages(p,dependencies=T,repos='http://cran.us.r-project.org')}
-#   library(p,character.only=T)    
+#   library(p,character.only=T)
 # }
-# 
+#
 # packagesgithub=c('easyassertions','basicepistats','nordcanepistats','nordcanpreprocessing')
 # for (p in packagesgithub){
 #   if (!require(p,character.only=T)){install_github("WetRobot/p",force=T)}
-#   library(p,character.only=T)    
+#   library(p,character.only=T)
 # }
-# 
+#
 # ###data
 # date_of_birth=sample(seq(as.Date('1900-01-01'),as.Date('1950-12-31'),by="day"),rep=T,10000)
 # date_of_incidence=date_of_birth+sample(as.numeric(difftime(as.Date('1953-01-01'),as.Date('1900-01-01'))):(as.numeric(difftime(as.Date('1953-01-01'),as.Date('1900-01-01')))+1000),rep=T,10000)
@@ -51,6 +51,7 @@
 #   autopsy=sample(0:1,10000,rep=T,prob=c(0.99,0.01))
 # )
 
+#' @importFrom data.table setDT copy := month year
 enrich_nordcan_cancer_case_dataset <- function(x) {
   dbc::assert_is_data.frame(x)
 
@@ -78,6 +79,75 @@ enrich_nordcan_cancer_case_dataset <- function(x) {
 
   return(x[])
 }
+
+
+
+#' @importFrom iarccrgtools create_example
+#' @importFrom nordcancore nordcan_column_name_set_names nordcan_column_name_set
+#' @importFrom data.table setDT setnames
+iarccrgtools_dataset <- function(
+  x,
+  tool_name
+) {
+  dbc::assert_is_character_nonNA_atom(tool_name)
+  iarccrgtools_column_name_set_names <-
+    nordcancore::nordcan_column_name_set_names()
+  iarccrgtools_column_name_set_names <- iarccrgtools_column_name_set_names[
+    grepl("iarccrgtools", iarccrgtools_column_name_set_names)
+  ]
+  tool_name_set <- sub("column_name_set_iarccrgtools_((all)|(mandatory))_",
+                       "",
+                       iarccrgtools_column_name_set_names)
+  tool_name_set <- unique(tool_name_set)
+  dbc::assert_atom_is_in_set(
+    tool_name,
+    set = tool_name_set
+  )
+  template <- iarccrgtools:::create_example(paste0("mandatory_", tool_name),
+                                            n.rows = 10L)
+  col_nms <- nordcancore::nordcan_column_name_set(
+    paste0("column_name_set_iarccrgtools_mandatory_", tool_name)
+  )
+  iarc_data <- data.table::setDT(lapply(col_nms, function(col_nm) {
+    tgt_class <- class(template[[col_nm]])[1L]
+    conversion_fun <- match.fun(paste0("as.", tgt_class))
+    conversion_fun(x[[col_nm]])
+  }))
+  data.table::setnames(iarc_data, names(iarc_data), names(col_nms))
+  return(iarc_data[])
+}
+
+
+#' @importFrom iarccrgtools set_tools_exe_path set_tools_work_dir
+#' interact_with_tool connect_tool_results_to_observations
+#' @importFrom data.table setkeyv
+iarccrgtools_tool <- function(
+  x,
+  tool_name,
+  iarccrgtools_exe_path,
+  iarccrgtools_work_dir
+  ) {
+  x <- iarccrgtools_dataset(x, tool_name = tool_name)
+
+  iarccrgtools::set_tools_exe_path(iarccrgtools_exe_path)
+  iarccrgtools::set_tools_work_dir(iarccrgtools_work_dir)
+  iarc_results <- iarccrgtools::interact_with_tool(
+    data = x,
+    tool.name = tool_name,
+    clean = FALSE,
+    verbose = TRUE
+  )
+
+  iarc_dt <- iarccrgtools::connect_tool_results_to_observations(
+    record.ids = x[["record_id"]],
+    tool.results = iarc_results
+  )
+  data.table::setkeyv(iarc_dt, "record_id")
+
+  return(iarc_dt[])
+}
+
+
 
 
 
