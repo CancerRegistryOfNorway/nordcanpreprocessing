ce8137f6fd064de2a5fdbddc4ddd8d1fe97d134c
Sasha Pejicic <52110058+the-r-man@users.noreply.github.com>
2020-09-07 16:08:40 +0200

    Update preprocessing_enrichment.R

--- R/preprocessing_enrichment.R
+++ R/preprocessing_enrichment.R
@@ -84,7 +84,8 @@ enrich_nordcan_cancer_case_dataset <- function(
     on = "tum",
     j = "excl_imp_error" := suppressWarnings(i.icdo3_to_icd10_input.eO3to10),
   ]
-
+  x[, "excl_imp_error" := ifelse (is.na(excl_imp_error),"0","1")]
+  
   return(x[])
 }
 
