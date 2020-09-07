4cfcdd285e7dd1c9f5b85a342df429b593dce0ab
Sasha Pejicic <52110058+the-r-man@users.noreply.github.com>
2020-09-07 14:15:51 +0200

    Update preprocessing_enrichment.R

--- R/preprocessing_enrichment.R
+++ R/preprocessing_enrichment.R
@@ -32,6 +32,9 @@
 # data$date_of_birth=as.Date(data$date_of_birth,format="%d.%m.%y")
 # data$date_of_incidence=as.Date(data$date_of_incidence,format="%d.%m.%y")
 # data$end_of_followup=as.Date(data$end_of_followup,format="%d.%m.%y")
+# data=data[,-which(names(data) %in% c("icd10","mob","yob","moi","mof","yof","surv_time","autopsy","period","excl_surv_age","excl_surv_dco",
+# "excl_surv_autopsy","excl_surv_negativefou","excl_surv_zerofou"))]
+
 
 #' @importFrom data.table setDT copy := month year
 enrich_nordcan_cancer_case_dataset <- function(
