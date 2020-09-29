9e9d2feb1b910d7c60b129e864feff11da839005
Sasha Pejicic <52110058+the-r-man@users.noreply.github.com>
2020-09-29 08:50:58 +0200

    Update preprocessing_enrichment.R
    in preprocessing_enrichment.R just some formal changes

--- R/preprocessing_enrichment.R
+++ R/preprocessing_enrichment.R
@@ -6,18 +6,34 @@ add_nordcan_entity_columns <- function(x) {
   x <- merge(x, icd10_to_entity_dt, by = "icd10")
   
   ####for most conversions (change entity for basal cell carcinomas, set entity for meningeomas (316), set entity for gliomas (317))
-dt11=do.call(expand.grid, list(morpho = seq(8090,8094), beh = 3, entity_level_30 = 888))
-dt12=do.call(expand.grid, list(morpho = seq(9530,9539), beh = seq(0,3), entity_level_30 = 316))
-dt13=do.call(expand.grid, list(morpho = seq(9380,9411), beh = seq(1,3), entity_level_30 = 317))
-dt14=do.call(expand.grid, list(morpho = seq(9414,9460), beh = seq(1,3), entity_level_30 = 317))
-dt15=do.call(expand.grid, list(morpho = 9390, beh = 0, entity_level_30 = 317))
-dt1=do.call(rbind, lapply(paste0("dt", 11:15), get))
+dt11 <- do.call(
+  expand.grid, list(morpho = seq(8090,8094), beh = 3, entity_level_30 = 888)
+               )
+dt12 <- do.call(
+  expand.grid, list(morpho = seq(9530,9539), beh = seq(0,3), entity_level_30 = 316)
+            )
+dt13 <- do.call(
+  expand.grid, list(morpho = seq(9380,9411), beh = seq(1,3), entity_level_30 = 317)
+            )
+dt14 <- do.call(
+  expand.grid, list(morpho = seq(9414,9460), beh = seq(1,3), entity_level_30 = 317)
+            )
+dt15 <- do.call(
+  expand.grid, list(morpho = 9390, beh = 0, entity_level_30 = 317)
+            )
+dt1 <- do.call(rbind, lapply(paste0("dt", 11:15), get))
 
 ####for limit included bladder/urinary tumors to 8010/2, 8120/1, 8120/2, 8130/1, 8130/2
-dt2=do.call(expand.grid, list(morpho = setdiff(unique(unprocessed_cancer_record_dataset$morpho),c(8010, 8120, 8130)), beh = seq(0,2), entity_level_30 = 280, new_entity_level_30=999))
+dt2=do.call(
+  expand.grid, list(
+    morpho = setdiff(unique(x$morpho),c(8010, 8120, 8130)), 
+    beh = seq(0,2), 
+    entity_level_30 = 280, 
+    new_entity_level_30=999)
+           )
 
   ####exceptions based on stata code here
-   x[
+  x[
  i = dt1,
  on = c("morpho", "beh"),
  j = "entity_level_30" := i.entity_level_30
